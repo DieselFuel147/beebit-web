e7efb4efa7bd925cbbcfc04d57b1b7e70b659f69
Winfried Holzapfel <wholzapfel@hotmail.com>
2019-04-13 14:20:38 +1000

    Added body detection OpenCL example

---
+++ bodydetect_opencv/main.cpp
@@ -0,0 +1,33 @@
+#include <opencv/opencv.hpp>
+
+#include <vector>
+
+using namespace cv;
+
+int main() {
+    // initialization
+    
+    VideoCapture vcap("../resources/vtest.avi");
+    CascadeClassifier fd("../resources/har_body.xml");
+    UMat frame, frameGray;
+    Mat canvas;
+    std::vector<Rect> faces;
+    while (1) {
+        // processing loop
+        vcap >> frame;
+        cvtColor(frame, frameGray, COLOR_BGR2GRAY);
+        equalizeHist(frameGray, frameGray);
+        fd.detectMultiScale(frameGray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30));
+      
+        frame.copyTo(canvas);
+        for( size_t i = 0; i < faces.size(); i++ ) {
+          rectangle(canvas, faces[i], Scalar(255, 0, 0));
+        }
+        
+        imshow("Face Detect", canvas);
+        
+        char c = (char) waitKey(30);
+        if( c == 27 || c == 'q' || c == 'Q' ) break;
+    }
+    return 0;
+}
