60ff805a573d3fe996a2766f28069e8299a7161d
Win Holzapfel <wholzapfel@hotmail.com>
2019-09-11 12:51:59 +1000

    Fix pkey not initializing

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,8 +5,8 @@ PORT=22
 DEPLOY_DIR=/var/www/dev.beebithive.com/beebit-web
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
+chmod 600 .travis/pkey # Allow read access to the private key
+ssh-add .travis/pkey # Add the private key to SSH
 
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
