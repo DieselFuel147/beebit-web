f4b2b0d4067ad89f74a5da36fbdefa3ba573f2af
Win Holzapfel <wholzapfel@hotmail.com>
2019-09-17 12:37:49 +1000

    Fix flot graphing

--- routes/demo.js
+++ routes/demo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET demo dashboard. */
 router.get('/', function(req, res, next) {
-  res.render('dash/starter', {
+  res.render('dash/index', {
     layout: 'dash/layout',
     title: 'BeeBit Dashboard'
   });
