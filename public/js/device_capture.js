ccd50a8767ac8ef022fa079ad5f42ac62032c38f
Winfried Holzapfel <wholzapfel@hotmail.com>
2019-10-23 15:24:52 +1100

    Client box editing

--- public/js/device_capture.js
+++ public/js/device_capture.js
@@ -1,6 +1,8 @@
 var config_options = {
     check_frequency: 1000, // How often to check for the image response, in milliseconds
-    aspect_ratio: 0.75
+    aspect_ratio: 0.75,
+    default_width: 0.075,
+    default_height: 0.1,
 }
 
 var hostname = location.protocol + "//" + location.host;
@@ -9,27 +11,30 @@ var beeId = "";
 var timeRequested = Math.floor((new Date()/1000));
 var interval;
 
+var canvasImg = null;
+
 // Box drawing attributes
 let canvas_w = 600;
 let canvas_h = 450;
+let canvas;
 let ctx;
 
 var selected_idx;
 
+// Bounding box management
 boxes = [];
+selectedBoxIndex = -1;
 
 function checkDeviceImage() {
     $.post(`${hostname}/bee/img/fetch/${beeId}`, function (data) {
-        console.log(timeRequested);
-        console.log(data.rtime);
         if (data.rtime > timeRequested) {
             $("#loadIndicator").hide();
-            var canvasImg = new Image(canvas_w, canvas_h);
+            canvasImg = new Image(canvas_w, canvas_h);
             canvasImg.src = "data:image/jpg;base64," + data.image;
 
             canvasImg.onload = function() {
                 // Draw the retrieved image to the canvas
-                ctx.drawImage(canvasImg, 0, 0, canvas_w, canvas_h);
+                redraw();
             }
 
             clearInterval(interval);
@@ -40,7 +45,7 @@ function checkDeviceImage() {
 $(document).ready(function() {
     beeId = $("#beeID")[0].innerHTML;
 
-    let canvas = $("#boxDraw")[0];
+    canvas = $("#boxDraw")[0];
     ctx = canvas.getContext("2d");
 
     canvas_w = $("#boxDraw").parent().width();
@@ -48,6 +53,9 @@ $(document).ready(function() {
 
     canvas.width = canvas_w;
     canvas.height = canvas_h;
+
+    // Register event handlers
+    canvas.addEventListener('mousedown', onMouseDown);
 
     // Fetch the boxes from the server and draw them
     //drawBoxes();
@@ -64,98 +72,70 @@ $(document).ready(function() {
     interval = setInterval(checkDeviceImage, config_options.check_frequency);
 });
 
-function drawBox(b) {
-    drawArea.append(b);
+function onSelectionChanged(box) {
+    console.log(box);
+
+    // When the selected box changes
+    $("#boxName").val(box.name);
+    $("#boxX").val(box.x);
+    $("#boxY").val(box.y);
+    $("#boxW").val(box.width);
+    $("#boxH").val(box.height);
 }
 
-function clearDrawArea() {
-    $("#graph").children().not("defs").remove();
-}
+function redraw() {
 
-function onBoxClick(e) {
-	e.preventDefault();
-	e.stopPropagation();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    idx = this.getAttribute('index');
-   
-    if (idx == selected_idx) {
-        boxes[idx].setAttribute("stroke", "black");
-        selected_idx = null;
-        removeDraggable(boxes[idx])
-    }
-    else {
-        if (selected_idx != null) {
-            boxes[selected_idx].setAttribute("stroke", "black");
-            removeDraggable(boxes[selected_idx])
-        }
-        boxes[idx].setAttribute("stroke", "red");
-        selected_idx = idx;
-        makeDraggable(boxes[idx])
-    }
-}
+    // Clear the canvas for drawing
+    if (canvasImg != null) ctx.drawImage(canvasImg, 0, 0, canvas_w, canvas_h);
 
+    // Style for drawing boxes
+    ctx.lineWidth = 6;
 
-function addRandomBox() {
-
-    let x = Math.random()  * (1 - .15);
-    let y = Math.random()  * (1 - .15);
-
-    let x2 = (Math.random() * (1 - x)) + x;
-    let y2 = (Math.random() * (1 - y)) + y;
-
-    let w = x2 - x;
-    let h = y2 - y;
-
-    //console.log("[x, y, x2, y2, w, h] " + x + " " + y + " " + x2 + " " + y2 + " " + w + " " + h + " ");
-
-    var svg = document.createElementNS("http://www.w3.org/2000/svg", "rect"); 
-    svg.setAttribute("x", x * canvas_w);
-    svg.setAttribute("y", y * canvas_h);
-    svg.setAttribute("width", w * canvas_w);
-    svg.setAttribute("height", h * canvas_h);
-
-    svg.setAttribute("stroke", "black");
-	svg.setAttribute("fill", "#d2d2d2");
-    svg.setAttribute("fill-opacity", "0.5");
-
-    svg.setAttribute('index', boxes.length);
-    svg.onclick = onBoxClick;
-
-    boxes.push(svg);
-    drawBox(svg);
-}
-
-function drawBoxes() {
-    clearDrawArea();
-    boxes.forEach(b => {
-        drawBox(b);
+    // Draw each box with a random colour, normalizing the coordinates
+    boxes.forEach((box, index) => {
+        ctx.beginPath();
+        ctx.strokeStyle = index == selectedBoxIndex ? "red" : "green";
+        ctx.rect(box.x*canvas_w, box.y*canvas_h, box.width*canvas_w, box.height*canvas_h);
+        ctx.stroke();
     });
 }
 
-/* Moveable svg */
-// http://www.petercollingridge.co.uk/tutorials/svg/interactive/dragging/
-function removeDraggable(e) {
-    e.removeEventListener('mousedown', startDrag);
-    e.removeEventListener('mousemove', drag);
-    e.removeEventListener('mouseup', endDrag);
-    e.removeEventListener('mouseleave', endDrag);
+function randomColor() {
+    return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
 }
 
-function makeDraggable(e) {
-    e.addEventListener('mousedown', startDrag);
-    e.addEventListener('mousemove', drag);
-    e.addEventListener('mouseup', endDrag);
-    e.addEventListener('mouseleave', endDrag);
-}
+function onMouseDown(event) {
+    const rect = canvas.getBoundingClientRect();
+    const normX = (event.clientX - rect.left) / canvas_w;
+    const normY = (event.clientY - rect.top) / canvas_h;
 
-var svg = null;
-function startDrag(e) {
-}
+    var foundBox = -1;
+    foundBox = boxes.findIndex(function (box) {
+        return box.x < normX 
+            && box.y < normY 
+            && (box.x + box.width) > normX 
+            && (box.y + box.height) > normY;
+    });
 
-function drag(e) {
-    e.preventDefault();
-    boxes[selected_idx].setAttribute('x', boxes[selected_idx].getAttribute('x') * 1.01);
-}
+    if (foundBox != -1) {
+        selectedBoxIndex = foundBox;
+        onSelectionChanged(boxes[selectedBoxIndex]);
+        redraw();
+        return;
+    }
 
-function endDrag(e) {
+    boxes.push({
+        name: "Area #" + boxes.length,
+        x: normX - config_options.default_width / 2,
+        y: normY - config_options.default_height  / 2,
+        width: config_options.default_width,
+        height: config_options.default_height
+    });
+
+    selectedBoxIndex = boxes.length-1;
+    onSelectionChanged(boxes[selectedBoxIndex]);
+
+    redraw();
 }
